6c6e09f17fdf95d4b731dea1ad9b7c9662be5fbf
Guilherme Risson <bppm8qd8nk@privaterelay.appleid.com>
2026-01-21 10:34:01 -0300

    Update executar_robo.bat

--- scripts/executar_robo.bat
+++ scripts/executar_robo.bat
@@ -1,4 +1,5 @@
 @echo off
 echo Iniciando o Robo...
-"C:\Users\Guilherme Risson\AppData\Local\Python\pythoncore-3.14-64\python.exe" "C:\Users\Guilherme Risson\OneDrive\Documents\Estudos Analytics\VSCode\scraperfinal.py"
+"Caminho da pasta no explorador de onde seu Python.exe foi instalado" "Caminho da pasta no explorador de onde salvou seu Script"
+
 
