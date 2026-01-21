587d3e8163e8d87540fee0d587b2071510390a8c
Guilherme Risson <bppm8qd8nk@privaterelay.appleid.com>
2026-01-21 10:03:27 -0300

    Update and rename executar_robo.bat to scripts/executar_robo.bat

--- scripts/executar_robo.bat
+++ scripts/executar_robo.bat
@@ -1,3 +1,4 @@
 @echo off
 echo Iniciando o Robo...
 "C:\Users\Guilherme Risson\AppData\Local\Python\pythoncore-3.14-64\python.exe" "C:\Users\Guilherme Risson\OneDrive\Documents\Estudos Analytics\VSCode\scraperfinal.py"
+
